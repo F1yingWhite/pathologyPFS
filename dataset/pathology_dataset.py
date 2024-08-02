2467f7ddd57bb9e97bdd2fc417813236e14c42c7
F1yingWhite <81723334@qq.com>
2024-08-02 15:24:17 +0800

    计算pt文件

--- dataset/pathology_dataset.py
+++ dataset/pathology_dataset.py
@@ -1,35 +1,31 @@
 import glob
 import os
-import warnings
 
-import numpy as np
-import timm
 import torch
-import torch.nn as nn
 import torch.utils.data as data
 from PIL import Image
-from torchvision import transforms
 
 
 class PathologyTileDataset(data.Dataset):
-    def __init__(self, img_path, transform=None):
+    def __init__(self, img_path, transform):
+        assert transform is not None, "transform is required"
+        self.transform = transform
         self.img_list = []
         self.coordinates_list = []
 
-        # 使用 glob 过滤出特定格式的图像文件（例如 jpg 和 png）
-        for img_file in glob.glob(os.path.join(img_path, '*.jpeg')) + glob.glob(os.path.join(img_path, '*.png')):
+        for img_file in glob.glob(os.path.join(img_path, '*.jpeg')):
             self.img_list.append(img_file)
             filename = os.path.basename(img_file).split(".")[0]
             coordinates = [int(coord) for coord in filename.split("_")[:2]]
             self.coordinates_list.append(coordinates)
 
-        self.transform = transform
+        assert len(self.img_list) > 0, "No images found in the given path"
 
     def __len__(self):
         return len(self.img_list)
 
     def __getitem__(self, index):
-        img = Image.open(self.img_list[index]).convert('RGB')  # 确保图像是 RGB 格式
+        img = Image.open(self.img_list[index]).convert('RGB')
         if self.transform:
             img = self.transform(img)
         return img, torch.tensor(self.coordinates_list[index])
